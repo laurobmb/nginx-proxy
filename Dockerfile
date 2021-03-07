fc86cca30ad3eadd55b618f2b95ecf214e109278
Lauro Gomes <laurobmb@gmail.com>
2021-03-07 16:54:12 -0300

    v2

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM nginx:latest
 
 COPY default.conf /etc/nginx/conf.d/
 
-#COPY app6.w0rm30.seg.br.crt /etc/ssl/app6.w0rm30.seg.br.crt
+#COPY app6.w0rm30.seg.br.crt /etc/ssl/app6.w0rm30.seg.br.pem
 #COPY app6.w0rm30.seg.br.key /etc/ssl/app6.w0rm30.seg.br.key
 
 COPY build_cert.sh /root
--- build_cert.sh
+++ build_cert.sh
@@ -2,12 +2,16 @@
 
 host="app6.w0rm30.seg.br"
 
+openssl genrsa -out rootCA.key 2048
+
+openssl req -out rootCA.pem -key rootCA.key -new -x509 -days 3650 -subj "/C=BR/ST=Pernambuco/L=Recife/O=Suporte Avancado/O=Security/CN=root.$host"
+
 openssl genrsa -out $host.key 2048
 
-openssl rsa -in $host.key -out $host.key.sem.senha
+openssl req -out $host.req -key $host.key -new -subj "/C=BR/ST=Pernambuco/L=Recife/O=Suporte Avancado/O=Security/CN=$host"
 
-openssl req -new -key $host.key -out $host.csr -subj "/C=BR/ST=Pernambuco/L=Recife/O=sv/O=TRF5/CN=$host"
+openssl x509 -in $host.req -out $host.pem -days 365 -req -CA rootCA.pem -CAkey rootCA.key -CAcreateserial
 
-openssl x509 -req -days 365 -in $host.csr -signkey $host.key -out $host.crt
+openssl verify -CAfile rootCA.pem -purpose sslserver $host.pem
 
-rm -f $host.csr $host.key.sem.senha
+rm -f $host.req $host.csr $host.key.sem.senha rootCA.srl
