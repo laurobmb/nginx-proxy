b20cb5fd7be1c981c040c861e81ccd2eb1889213
Lauro Gomes <laurobmb@gmail.com>
2021-03-08 02:08:26 -0300

    v5

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,6 @@
 FROM nginx:latest
-
 COPY default.conf /etc/nginx/conf.d/
-
-#COPY app6.w0rm30.seg.br.crt /etc/ssl/app6.w0rm30.seg.br.pem
-#COPY app6.w0rm30.seg.br.key /etc/ssl/app6.w0rm30.seg.br.key
-
+ENV host=app6.w0rm30.seg.br
 COPY build_cert.sh /root
-
 RUN cd /etc/ssl/ &&\
-    bash /root/build_cert.sh
-
+    bash /root/build_cert.sh $host
--- build_cert.sh
+++ build_cert.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-
-host="app6.w0rm30.seg.br"
-
+host=$1
 openssl genrsa -out rootCA.key 2048
-
 openssl req -out rootCA.pem -key rootCA.key -new -x509 -days 3650 -subj "/C=BR/ST=Pernambuco/L=Recife/O=Suporte Avancado/O=Security/CN=root.$host"
-
 openssl genrsa -out $host.key 2048
-
 openssl req -out $host.req -key $host.key -new -subj "/C=BR/ST=Pernambuco/L=Recife/O=Suporte Avancado/O=Security/CN=$host"
-
 openssl x509 -in $host.req -out $host.pem -days 365 -req -CA rootCA.pem -CAkey rootCA.key -CAcreateserial
-
 openssl verify -CAfile rootCA.pem -purpose sslserver $host.pem
-
 rm -f $host.req $host.csr $host.key.sem.senha rootCA.srl
